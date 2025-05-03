1c9c12b0cdd0b7ae840fcc077f640d85fcfe074e
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-03 12:17:50 +0100

    wk3 lct2 assignment done & deployed and verified

---
+++ Assignments/week 3/lct 2/scripts/deploy.ts
@@ -0,0 +1,16 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Counter = await ethers.deployContract("VotingSystem");
+
+  await Counter.waitForDeployment();
+
+  console.log("Counter Contract Deployed at " + Counter.target);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
