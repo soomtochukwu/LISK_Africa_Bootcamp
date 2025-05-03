72161ebc0d739c18750eff20607b30de32879c3a
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-05-03 08:45:54 +0100

    attempted wk 3 assignments

---
+++ Assignments/week 3/lct 1/contracts/ToDo.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract TodoList {
+    enum Status {
+        Pending,
+        InProgress,
+        Completed
+    }
+
+    struct Todo {
+        uint id;
+        string content;
+        Status status;
+    }
+
+    event TodoAdded(uint id, string content);
+
+    // I introduced this variable to track Todo-tasks IDs.
+    // It starts from 1.
+    uint private idCounter = 1;
+
+    mapping(uint => Todo) public tasks;
+    uint[] public todoIds;
+
+    function addTodo(string memory _content) public {
+        tasks[idCounter] = Todo({
+            id: idCounter,
+            content: _content,
+            status: Status.Pending
+        });
+
+        emit TodoAdded(idCounter, _content);
+        // I would increment the counter each time i add a Todo task.
+        idCounter += 1;
+    }
+
+    function updateStatus(uint _id, Status _status) public {
+        tasks[_id].status = _status;
+    }
+
+    function getTodo(uint _id) public view returns (Todo memory) {
+        return tasks[_id];
+    }
+}
---
+++ Assignments/week 3/lct 2/contracts/VotingSystem.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Ownable {
+    address public owner;
+    modifier OnlyOwner() {
+        require(msg.sender == owner, "ACCESS DENIED!!!");
+        _;
+    }
+}
+
+contract VotingSystem is Ownable {
+    // I introduced this variable to track IDs.
+    // It starts from 1.
+    uint private idCounter = 1;
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public hasVoted;
+
+    mapping(address => bool) voters;
+
+    constructor() {
+        // this is where I initialized the owner variable that was inherited form the base contract
+        owner = msg.sender;
+    }
+
+    function addCandidate(string calldata name) public OnlyOwner {
+        candidates[idCounter] = Candidate({
+            id: idCounter,
+            name: name,
+            voteCount: 0
+        });
+
+        // I update idCounter each time i add a new candidate.
+        idCounter += 1;
+    }
+
+    function vote(uint candidateId) public {
+        require(!voters[msg.sender], "YOU CAN ONLY VOTE ONCE!!");
+        candidates[candidateId].voteCount += 1;
+    }
+
+    function getCandidate(
+        uint candidateId
+    ) public view returns (string memory name, uint voteCount) {
+        return (
+            candidates[candidateId].name,
+            candidates[candidateId].voteCount
+        );
+    }
+
+    function getTotalCandidates() public view returns (uint) {
+        // This should work :).
+        return idCounter - 1;
+    }
+}
