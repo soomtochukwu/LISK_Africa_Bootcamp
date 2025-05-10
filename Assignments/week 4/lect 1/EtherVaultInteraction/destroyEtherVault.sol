8cce755098eb9d223f84ecee06e22421212c7067
MaziOfWeb3 <142386413+soomtochukwu@users.noreply.github.com>
2025-05-10 14:06:34 +0100

    Update destroyEtherVault.sol

--- Assignments/week 4/lect 1/EtherVaultInteraction/destroyEtherVault.sol
+++ Assignments/week 4/lect 1/EtherVaultInteraction/destroyEtherVault.sol
@@ -11,7 +11,7 @@ contract SelfDestruct_byMazi {
     function destroyEtherVault(address EtherVaultAddress) payable external {
         require(msg.sender == owner, "Not authorized");
         // require(msg.value == (0.0005 * 1e18) wei, "Must send exactly 0.0005 ether");
-        require((msg.value >= (0.0005 * 1e18)) && ((msg.value <= (0.0005 * 1e18))), "Not enough or too much Ether sent for selfdestruct.");
+        require((msg.value >= (0.0005 * 1e18)) && ((msg.value <= (0.0005 * 1e18))), "Must send exactly 0.0005 ether");
         selfdestruct((payable(EtherVaultAddress)));
     }
 }
